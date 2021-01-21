472e926d6c5595be169cc822a654a84b75ae8955
hylas-develop <74232110+hylas-develop@users.noreply.github.com>
2021-01-21 23:51:59 +0900

    Falling in Love with Admin Panel

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from . import models
-# Register your models here.
 
 
-@admin.register(models.User)
+@admin.register(models.User)  # == admin.site.register(models.User, CustomUserAdmin)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    """ Custom  User Admin """
+
+    # How to display User Info in admin panel
+    list_display = ('username', "email", 'gender', 'language', 'currency', 'superhost')
+    # Create Filter
+    list_filter = ("language", "currency", "superhost")
