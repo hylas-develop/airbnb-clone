254f7713ac5a2336ad44e3900108d5917f57c3fb
hylas-develop <74232110+hylas-develop@users.noreply.github.com>
2021-01-22 00:10:09 +0900

    UserAdmin + CustomAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
 @admin.register(models.User)  # == admin.site.register(models.User, CustomUserAdmin)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     """ Custom  User Admin """
-
-    # How to display User Info in admin panel
-    list_display = ('username', "email", 'gender', 'language', 'currency', 'superhost')
-    # Create Filter
-    list_filter = ("language", "currency", "superhost")
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
